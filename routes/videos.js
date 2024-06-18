500457464343881306bbc87f2a728a56907cf927
Lindsay Renna <lrenna.oct@gmail.com>
2024-06-17 21:27:02 -0400

    POST request functionality complete

--- routes/videos.js
+++ routes/videos.js
@@ -1,4 +1,5 @@
 import express from "express";
+import uniqid from "uniqid";
 import fs from "fs";
 const router = express.Router();
 
@@ -8,6 +9,12 @@ function readVideoDetails() {
 	const videosFile = fs.readFileSync("data/videos.json");
 	const videosDetails = JSON.parse(videosFile);
 	return videosDetails;
+}
+
+// function to write or add to our JSON file
+function addVideo(data) {
+	const stringifiedData = JSON.stringify(data);
+	fs.writeFileSync("data/videos.json", stringifiedData);
 }
 
 router.get("/", (_req, res) => {
@@ -33,6 +40,26 @@ router.get("/:id", (req, res) => {
 	} else {
 		res.status(404).send("No video with that id exists");
 	}
+});
+
+router.post("/", (req, res) => {
+	const videoDetails = readVideoDetails();
+	const newVideo = {
+		id: uniqid(),
+		title: req.body.title,
+		channel: "",
+		image: req.body.image,
+		description: req.body.description,
+		views: "0",
+		likes: "0",
+		duration: "5:35",
+		video: "https://unit-3-project-api-0a5620414506.herokuapp.com/stream",
+		timestamp: req.body.timestamp,
+		comments: [],
+	};
+	videoDetails.push(newVideo);
+	addVideo(videoDetails);
+	res.status(201).json(newVideo);
 });
 
 export default router;
